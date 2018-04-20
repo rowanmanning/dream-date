1c8d40b90082da3cc2758c24d736320d139e3cba
Rowan Manning <accounts+github@rowanmanning.com>
2018-04-20 19:56:41 +0100

    Fix the common Harptos calendar

--- calendar/schema/harptos-common.js
+++ calendar/schema/harptos-common.js
@@ -7,15 +7,15 @@ const months = schema.calendar.year.months;
 months[0].name = 'Deepwinter';
 months[0].long = 'The Month of Deepwinter';
 
-months[2].name = 'The Claw of Winter';
+months[2].name = months[2].long = 'The Claw of Winter';
 
-months[3].name = 'The Claw of the Sunsets';
+months[3].name = months[3].long = 'The Claw of the Sunsets';
 
-months[4].name = 'The Claw of the Storms';
+months[4].name = months[4].long = 'The Claw of the Storms';
 
-months[6].name = 'The Melting';
+months[6].name = months[6].long = 'The Melting';
 
-months[7].name = 'The Time of Flowers';
+months[7].name = months[7].long = 'The Time of Flowers';
 
 months[8].name = 'Summertide';
 months[8].long = 'The Month of Summertide';
@@ -23,11 +23,11 @@ months[8].long = 'The Month of Summertide';
 months[11].name = 'Highsun';
 months[11].long = 'The Month of Highsun';
 
-months[12].name = 'The Fading';
+months[12].name = months[12].long = 'The Fading';
 
 months[14].name = 'Leaffall';
 months[14].long = 'The Month of Leaffall';
 
-months[15].name = 'The Rotting';
+months[15].name = months[15].long = 'The Rotting';
 
-months[17].name = 'The Drawing Down';
+months[17].name = months[17].long = 'The Drawing Down';
