a733e1a3a87131fd95388c5192ac3751db3ffbca
Rowan Manning <accounts+github@rowanmanning.com>
2018-04-20 18:19:52 +0100

    Add long names for Harptos months

--- calendar/schema/harptos.js
+++ calendar/schema/harptos.js
@@ -14,75 +14,93 @@ module.exports = {
 			months: [
 				{
 					name: 'Hammer',
+					long: 'Month of Hammer',
 					days: 30
 				},
 				{
 					name: 'Midwinter',
+					long: 'Festival of Midwinter',
 					days: 1
 				},
 				{
 					name: 'Alturiak',
+					long: 'Month of Alturiak',
 					days: 30
 				},
 				{
 					name: 'Ches',
+					long: 'Month of Ches',
 					days: 30
 				},
 				{
 					name: 'Tarsakh',
+					long: 'Month of Tarsakh',
 					days: 30
 				},
 				{
 					name: 'Greengrass',
+					long: 'Festival of Greengrass',
 					days: 1
 				},
 				{
 					name: 'Mirtul',
+					long: 'Month of Mirtul',
 					days: 30
 				},
 				{
 					name: 'Kythorn',
+					long: 'Month of Kythorn',
 					days: 30
 				},
 				{
 					name: 'Flamerule',
+					long: 'Month of Flamerule',
 					days: 30
 				},
 				{
 					name: 'Midsummer',
+					long: 'Festival of Midsummer',
 					days: 1
 				},
 				{
 					name: 'Shieldmeet',
+					long: 'Shieldmeet',
 					days: 0,
 					daysInLeapYear: 1
 				},
 				{
 					name: 'Eleasis',
+					long: 'Month of Eleasis',
 					days: 30
 				},
 				{
 					name: 'Eleint',
+					long: 'Month of Eleint',
 					days: 30
 				},
 				{
 					name: 'Highharvestide',
+					long: 'Festival of Highharvestide',
 					days: 1
 				},
 				{
 					name: 'Marpenoth',
+					long: 'Month of Marpenoth',
 					days: 30
 				},
 				{
 					name: 'Uktar',
+					long: 'Month of Uktar',
 					days: 30
 				},
 				{
 					name: 'Feast of the Moon',
+					long: 'The Feast of the Moon',
 					days: 1
 				},
 				{
 					name: 'Nightal',
+					long: 'Month of Nightal',
 					days: 30
 				}
 			]
