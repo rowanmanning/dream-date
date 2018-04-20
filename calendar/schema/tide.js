53ad73177e725f3c71889fbdd43ca6a994f3814a
Rowan Manning <accounts+github@rowanmanning.com>
2018-04-20 18:17:48 +0100

    Remove some of the abbreviations from Tide calendar

--- calendar/schema/tide.js
+++ calendar/schema/tide.js
@@ -6,73 +6,61 @@ module.exports = {
 			months: [
 				{
 					name: 'Wolfhunt',
-					abbr: 'Whu',
 					long: 'Month of Wolfhunt',
 					days: 30
 				},
 				{
 					name: 'Longdark',
-					abbr: 'Ldk',
 					long: 'Month of Longdark',
 					days: 30
 				},
 				{
 					name: 'Patience',
-					abbr: 'Pat',
 					long: 'Month of Patience',
 					days: 30
 				},
 				{
 					name: 'Thaw',
-					abbr: 'Thw',
 					long: 'Month of Thaw',
 					days: 30
 				},
 				{
 					name: 'Wellspring',
-					abbr: 'Wsp',
 					long: 'Month of Wellspring',
 					days: 30
 				},
 				{
 					name: 'Sunspeak',
-					abbr: 'Spk',
 					long: 'Month of Sunspeak',
 					days: 30
 				},
 				{
 					name: 'Midsomer',
-					abbr: 'Mso',
 					long: 'Month of Midsomer',
 					days: 30
 				},
 				{
 					name: 'Harvest',
-					abbr: 'Hvs',
 					long: 'Month of Harvest',
 					days: 30
 				},
 				{
 					name: 'Lateharvest',
-					abbr: 'Lhv',
 					long: 'Month of Lateharvest',
 					days: 30
 				},
 				{
 					name: 'Goldleaf',
-					abbr: 'Glf',
 					long: 'Month of Goldleaf',
 					days: 30
 				},
 				{
 					name: 'Deadleaf',
-					abbr: 'Dlf',
 					long: 'Month of Deadleaf',
 					days: 30
 				},
 				{
 					name: 'Yearsend',
-					abbr: 'Yre',
 					long: 'Month of Yearsend',
 					days: 30
 				}
